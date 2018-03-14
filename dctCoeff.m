85ad21c602089e39ca0e70239b04e2222352eabe
Victoria Hall <vhall001@ucr.edu>
2018-03-13 18:44:57 -0700

    added reverse dct function

--- dctCoeff.m
+++ dctCoeff.m
@@ -4,6 +4,8 @@ function [ coeff ] = dctCoeff( im, block_sz )
     if mod(480,block_sz) == 0 && mod(640,block_sz) == 0
         coeff = zeros(480/block_sz,640/block_sz);
         im_dct = dct2(im);
+        figure;imshow(im_dct);
+        title('dct image');
         for it=0:(480/block_sz)-1
            for jt=0:(640/block_sz)-1
                coeff(it+1,jt+1) = mean(mean(im_dct((it*block_sz)+1:(it+1)*block_sz,(jt*block_sz)+1:(jt+1)*block_sz)));
